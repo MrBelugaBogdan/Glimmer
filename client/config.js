79722ca6fd3060cbabf90b88e548a94bae4a082a
MrBelugaBogdan <medvedovbogdan19@gmail.com>
2026-03-25 14:55:04 +0300

    Delete client directory

--- client/config.js
+++
@@ -1,6 +0,0 @@
-export const GlimmerConfig = {
-    // Коли купиш сервер, заміниш це на "http://твій-ip-адреса:8080"
-    api_url: "http://localhost:8080", 
-    version: "0.3.0-Alpha",
-    isOnline: false 
-};
